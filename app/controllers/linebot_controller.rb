65076137a87c4db4daefc066993b215cb7c708b4
屋比久拓海 <yabikutakumi@yabikutakuminoMacBook-puro.local>
2019-08-09 13:54:24 +0900

    callbacks ルーティングを作成

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -101,6 +101,103 @@ message = {
     head :ok
   end
 
+  protect_from_forgery :except => [:callback]
+  def callbacks
+    body = request.body.read
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+  #  unless client.validate_signature(body, signature)
+  #    error 400 do 'Bad Request' end
+  #  end
+    events = client.parse_events_from(body)
+
+    array = {}
+    message = {}
+    events.each do |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          ##ぐるナビAPI叩いて返す
+
+          
+         uri   = "https://api.gnavi.co.jp/RestSearchAPI/20171214/"
+
+	acckey= "6afcd32694c4130f6a1ce4a7dc21a98b"
+
+	format= "json"
+	#lat   = 35.670083
+	#lon   = 139.763267
+	#range = 1
+	freeword = event.message['text']
+	#prefname = event.message['text']
+	p event.message['text'] 
+	#range = open(url)
+	
+	#line = {
+         #   type: 'text',
+          #  text: event.message['text']
+           # }
+
+	url  = sprintf("%s%s%s%s%s%s%s", uri, "?format=", format, "&keyid=", acckey, "&freeword=", freeword)
+	url = URI.encode url
+	p url
+	json = open(url)
+	#array = {}
+	json.each do |j|
+	    array = JSON.parse(j)
+	end
+
+	msg = ''
+        columns = [];
+	array["rest"].each do |rest|
+	     #puts rest["name"]
+	     msg = msg + rest["name"] 
+	     message = { 'type' => 'text', 'text' => rest["name"] }
+#	     response = client.push_message("Ub8c67cfce315de9e3f841e7a2136f5a8", message)
+	     #puts response
+             columns.push({
+                 "thumbnailImageUrl": rest["image_url"]["shop_image1"],
+                 "title": rest["name"],
+                 "text": rest["tel"],
+                 "actions": [
+                 {
+                     "type": "uri",
+                     "label": "電話する",
+                     "uri": "tel://" + rest["tel"]
+		 },
+                 {
+                     "type": "uri",
+                     "label": "お店のページへ",
+                     "uri": rest["url"]
+                 }]
+             });
+            #message = {
+            #type: 'text',
+            #text: event.message['text']
+            #}
+	end
+
+message = {
+    "type": "template",
+    "altText": "this is a carousel template",
+    "template": {
+        "type": "carousel",
+        "columns": columns
+    }
+}
+          #message = {
+            #type: 'text',
+            #text: event.message['text']
+           #}
+        end
+      end
+
+      #p array
+      client.reply_message(event['replyToken'], message)
+    end
+    head :ok
+  end
+
 private
 
 # LINE Developers登録完了後に作成される環境変数の認証
