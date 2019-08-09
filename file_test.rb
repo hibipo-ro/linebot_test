a95f55a932d1cc2018fdf3f72a050f51a751fce8
Hibiki Nitta <hi_bi_0120@icloud.com>
2019-08-09 11:22:00 +0900

    ファイルの書き込み、読み込みテスト

---
+++ file_test.rb
@@ -0,0 +1,3 @@
+File.open("text_write_test.txt","w") do |text|
+ text.puts gets
+end
--- line_file.rb
+++ line_file.rb
@@ -1,8 +1,8 @@
 class LinebotController < ApplicationController
   require 'line/bot'
 
-  require 'open-uri'
-  require 'json'
+  # require 'open-uri'
+  # require 'json'
 
   protect_from_forgery :except => [:callback]
   def callback
@@ -18,34 +18,6 @@ class LinebotController < ApplicationController
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
-          ##ぐるナビAPI叩いて返す
-
-          
-  uri   = "https://api.gnavi.co.jp/RestSearchAPI/20171214/"
-
-	acckey= "6afcd32694c4130f6a1ce4a7dc21a98b"
-
-	format= "json"
-	lat   = 35.670083
-	lon   = 139.763267
-	range = 1
-
-
-	url  = sprintf("%s%s%s%s%s%s%s%s%s%s%s", uri, "?format=", format, "&keyid=", acckey, "&latitude=", lat,"&longitude=",lon,"&range=",range)
-
-	json = open(url)
-	array = {}
-	json.each do |j|
-	    array = JSON.parse(j)
-	end
-
-	array["rest"].each do |rest|
-	     #puts rest["name"]
-	     message = { 'type' => 'text', 'text' => rest["name"] }
-	     response = client.push_message("Ub8c67cfce315de9e3f841e7a2136f5a8", message)
-	     puts response
-	end
-
 
           message = {
             type: 'text',
